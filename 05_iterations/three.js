9268d24192aeda09bd5af4083f878a40e5b5dd27
Pushplata27 <93395631+Pushplata27@users.noreply.github.com>
2023-09-19 11:59:46 +0000

    added some iterations files

---
+++ 05_iterations/three.js
@@ -0,0 +1,25 @@
+// for of
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
